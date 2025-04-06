179ac0898e33c497842f4e1fd92f3575112b58f8
Dinesh Gavai <gavaidinesh26@gmail.com>
2025-04-06 11:09:30 +0530

    Generate Test model integrated

--- AI_Feedback/settings.py
+++ AI_Feedback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8sireu61hhs0(=q80u1m64=%d-jb!2r07ctxt)mugv^z_ehf*0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["teachbot.onrender.com"]
+ALLOWED_HOSTS = ["teachbot.onrender.com","127.0.0.1","localhost"]
 
 # Application definition
 
